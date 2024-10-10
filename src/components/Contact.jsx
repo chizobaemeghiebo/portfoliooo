64bb25916a22907adcc8c179f62b47f641c470df
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-10-10 13:25:30 +0100

    removed custom form validations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,22 +10,22 @@ const Contact = () => {
     AOS.init()
   }, [])
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   message: '',
+  // })
 
-  const [errors, setErrors] = useState({})
+  // const [errors, setErrors] = useState({})
 
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-    // console.log(value)
-  }
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value,
+  //   })
+  //   // console.log(value)
+  // }
 
   // const handleSubmit = (e) => {
   //   e.preventDefault()
@@ -77,22 +77,19 @@ const Contact = () => {
               <label className='absolute text-xs -top-2 left-1 bg-pink-800 px-1 rounded-sm text-white' htmlFor='name'>
                 Name
               </label>
-              <input className='w-full rounded-sm p-2 bg-transparent border border-pink-400 focus:ring-blue-800' type='text' id='name' name='name' value={formData.name} onChange={handleChange} required />
-              {errors.name && <span className='text-xs block text-left mt-2 text-red-600 font-bold '>{errors.name}</span>}
+              <input className='w-full rounded-sm p-2 bg-transparent border border-pink-400 focus:ring-blue-800' type='text' id='name' name='name' required />
             </div>
             <div className='relative my-6 text-white'>
               <label className='absolute text-xs -top-2 left-1 bg-pink-800 px-1 rounded-sm text-white' htmlFor='email'>
                 Email
               </label>
-              <input className='w-full rounded-sm p-2 bg-transparent border border-pink-400 focus:ring-blue-800' type='email' id='email' name='email' value={formData.email} onChange={handleChange} required />
-              {errors.email && <span className='text-xs block text-left mt-2 text-red-600 font-bold '>{errors.email}</span>}
+              <input className='w-full rounded-sm p-2 bg-transparent border border-pink-400 focus:ring-blue-800' type='email' id='email' name='email' required />
             </div>
             <div className='relative my-6 text-white'>
               <label className='absolute text-xs -top-2 left-1 bg-pink-800 px-1 rounded-sm text-white' htmlFor='message'>
                 Message
               </label>
-              <textarea className='w-full rounded-sm p-2 bg-transparent border border-pink-400 h-52' type='text' id='message' name='message' value={formData.message} onChange={handleChange} required></textarea>
-              {errors.message && <span className='text-xs block text-left mt-2 text-red-600 font-bold'>{errors.message}</span>}
+              <textarea className='w-full rounded-sm p-2 bg-transparent border border-pink-400 h-52' type='text' id='message' name='message' required></textarea>
             </div>
             <div className='bg-pink-800 p-2 rounded-full text-white cursor-pointer '>
               <button type='submit' className='font-bold'>
