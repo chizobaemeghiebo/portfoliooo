6ce423921dc819bfe682ffbed4bbdc4b3f1c97a0
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-10-10 13:20:05 +0100

    removed custom form validations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,54 +27,51 @@ const Contact = () => {
     // console.log(value)
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const newErrors = validateForm(formData)
-    setErrors(newErrors)
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   const newErrors = validateForm(formData)
+  //   setErrors(newErrors)
 
-    if (Object.keys(newErrors).length === 0) {
-      // Form submission logic here
-      formData.name = ''
-      formData.email = ''
-      formData.message = ''
-      console.log('Form submitted successfully!')
-    } else {
-      console.log('Form submission failed due to validation errors.')
-    }
-  }
+  //   if (Object.keys(newErrors).length === 0) {
+  //     // Form submission logic here
+  //     formData.name = ''
+  //     formData.email = ''
+  //     formData.message = ''
+  //     console.log('Form submitted successfully!')
+  //   } else {
+  //     console.log('Form submission failed due to validation errors.')
+  //   }
+  // }
 
-  const validateForm = (data) => {
-    const errors = {}
+  // const validateForm = (data) => {
+  //   const errors = {}
 
-    if (!data.name.trim()) {
-      errors.name = 'Name is required'
-    } else if (data.name.length < 4) {
-      errors.name = 'Name must be at least 4 characters long'
-    }
+  //   if (!data.name.trim()) {
+  //     errors.name = 'Name is required'
+  //   } else if (data.name.length < 4) {
+  //     errors.name = 'Name must be at least 4 characters long'
+  //   }
 
-    if (!data.email.trim()) {
-      errors.email = 'Email is required'
-    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
-      errors.email = 'Email is invalid'
-    }
+  //   if (!data.email.trim()) {
+  //     errors.email = 'Email is required'
+  //   } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+  //     errors.email = 'Email is invalid'
+  //   }
 
-    if (!data.message) {
-      errors.message = 'Input your message'
-    } else if (data.message.length > 200) {
-      errors.message = 'Your message should not be more than 200 characters long'
-    }
-    return errors
-  }
+  //   if (!data.message) {
+  //     errors.message = 'Input your message'
+  //   } else if (data.message.length > 200) {
+  //     errors.message = 'Your message should not be more than 200 characters long'
+  //   }
+  //   return errors
+  // }
 
   return (
-    // TODO: FIGURE OUT WHY BACKGROUND IMAGE DOESN'T WORK
-    // style={{ backgroundImage: `url(${bg})` }}
-
     <section className='py-10 bg-[#1B2021]' id='contact'>
       <div data-aos='zoom-in-up' className='max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 '>
         <h3 className='text-white font-bold text-3xl text-center mb-4'>Contact Me</h3>
         <div className='text-center mx-auto bg-pink-800 max-w-xl backdrop-filter backdrop-blur-sm bg-opacity-20 rounded-md'>
-          <form className='  p-4 rounded-md shadow-lg' onSubmit={handleSubmit} netlify name='contact-form' method='POST'>
+          <form className=' p-4 rounded-md shadow-lg' netlify name='contact-form' method='POST'>
             <input type='hidden' name='form-name' value='contact-form' />
             <div className='relative my-6 text-white'>
               <label className='absolute text-xs -top-2 left-1 bg-pink-800 px-1 rounded-sm text-white' htmlFor='name'>
